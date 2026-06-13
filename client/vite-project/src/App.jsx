77e3b29f50c2680f34b1834284868e5a8d297a45
Muskan <muskankumar952593@gmail.com>
2026-06-13 13:22:45 +0530

    Update App.jsx

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -8,7 +8,7 @@ import Generate from './pages/Generate'
 import WebsiteEditor from './pages/Editor'
 import LiveSite from './pages/LiveSite'
 import Pricing from './pages/Pricing'
-export const serverUrl='http://localhost:8000'
+export const serverUrl='https://website-builder-20vp.onrender.com'
 
 function App() {
   useGetCurrentUser()
