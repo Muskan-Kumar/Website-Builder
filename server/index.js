a0c063d1f21c28c452dac71d2f995d92238d0ec2
Muskan <muskankumar952593@gmail.com>
2026-06-13 15:21:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://website-builder-2-mm6h.onrender.com",
     credentials:true
 }))
 
